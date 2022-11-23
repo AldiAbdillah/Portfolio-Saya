d746f2bdcc493265c26e2b667a00af9aa448f129
AldiAbdillah <you@example.com>
2022-11-23 19:44:45 +0700

    update dark mode + perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,12 @@
 //   plugins: [],
 // };
 
+// absolute right-4 top-full hidden w-full max-w-[250px] rounded-lg bg-white py-5 shadow-lg dark:bg-dark dark:shadow-slate-500 lg:static lg:block lg:max-w-full lg:rounded-none lg-shadow-none lg:dark:bg-transparent lg:dark:shadow-none
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -24,7 +27,7 @@ module.exports = {
       colors: {
         primary: "#3b82f6",
         secondary: "#b32b40",
-        dark: "#1e293b",
+        dark: "#0f172a",
       },
       screens: {
         "2xl": "1320px",
